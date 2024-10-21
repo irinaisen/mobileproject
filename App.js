741ca3f1c455d00d8ef97b492b9fd466fff78723
Jenna <jenna21013@student.hamk.fi>
2024-10-21 22:38:25 +0300

    Added functionality to select from map and dropdown
    - Home screen has state with the selected station

--- App.js
+++ App.js
@@ -1,70 +1,106 @@
-import React, {useState} from 'react';
+import React, { useRef, useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View, FlatList } from 'react-native';
-import {junienTiedot, liveTrains, trainLocations, stations} from './components/StaticApis';
+import { junienTiedot, liveTrains, trainLocations, stations } from './components/StaticApis';
 import MapWithMarkers from './components/Map';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useRoute } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DropdownComponent from './components/DropDownView';
-import {init} from './database/db';
+import { init } from './database/db';
 import styles from './views/styles'
 import HomeScreen from './components/HomeScreen';
 import NavButtons from './components/NavButtons';
 
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+//const route = useRoute();
 
 const Stack = createNativeStackNavigator();
 
 
 init()
-.then(()=>{
+  .then(() => {
     console.log('Database creation succeeded!');
-}).catch((err)=>{
-  console.log('Database IS NOT initialized! '+err);
-});
+  }).catch((err) => {
+    console.log('Database IS NOT initialized! ' + err);
+  });
 
+const selectStation = (shortCode) => {
+  console.log(shortCode)
+  //Miten saadaan stateen tieto että on valittu asema
 
+}
 
-const App=() => {
+const App = () => {
+
+  const [station, setStation] = useState('')
 
 
 
   return (
 
-  <NavigationContainer>
+    <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen1} />
         <Stack.Screen name="Map" component={MapScreen} />
         <Stack.Screen name="List" component={ListScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-   )
+  )
 }
 const HomeScreen1 = (props) => {
-    return (
-      
-      <View style={styles.container}><HomeScreen/>
-      <NavButtons params={props}></NavButtons>
-      </View>
 
-    )
+
+  // Sets the station based on navigation input (from map or from list (TODO))
+  const [selectedStation, setSelectedStation] = useState('Asema');
+  console.log(selectedStation)
+
+  if (props.route.params) {
+    let { station } = props.route.params;
+    console.log(station)
+    if (station !== selectedStation) {
+      setSelectedStation(station)
+    }
+  }
+
+  else {
+    console.log(props.route.params)
+    console.log('no params')
+  }
+
+  return (
+
+    <View style={styles.container}>
+      <Text style={{ color: '#fff', fontSize: 20 }}>{selectedStation}</Text>
+      <HomeScreen />
+      <NavButtons params={props}></NavButtons>
+    </View>
+
+  )
 }
 
 const MapScreen = (props) => {
   return (
-    <View style={styles.container}><MapWithMarkers/>
-    <NavButtons params={props}></NavButtons>
+    <View style={styles.container}>
+      <MapWithMarkers onMarkerSelect={(id, title) => {
+
+        props.navigation.navigate('Home', {
+          station: title,
+          shortCode: id
+        })
+      }} />
+      <NavButtons params={props}></NavButtons>
     </View>
-    
+
   )
 }
 
 const ListScreen = (props) => {
   return (
-    <View style={styles.container}><DropdownComponent/>
-    <NavButtons params={props}></NavButtons>
+    <View style={styles.container}><DropdownComponent navigation={props.navigation} />
+      <NavButtons params={props}></NavButtons>
     </View>
   )
 }
@@ -81,7 +117,7 @@ const ListScreen = (props) => {
 
 
 */
- 
+
 
 
 export default App;
--- components/DropDownView.js
+++ components/DropDownView.js
@@ -1,139 +1,150 @@
 import React, { useState, useEffect } from 'react';
-  import { StyleSheet, View, Text, FlatList } from 'react-native';
-  import { Dropdown } from 'react-native-element-dropdown';
-  import AntDesign from 'react-native-vector-icons/AntDesign';
-  import {liveTrains, stations} from './StaticApis';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import { liveTrains, stations } from './StaticApis';
 
 
-  const DropdownComponent = () => {
-    const [value, setValue] = useState(null); // Tallennetaan valittu dropdown valikon arvo
-    const [junaData, setJunaData] = useState([]); // Tallennetaan junatiedot flatlistiin.
-    const [asemaDataValikkoon, setAsemaData] = useState([]); // Tallennetaan asematiedot dropdown valikkoa varten
+const DropdownComponent = ({ navigation }) => {
+  const [value, setValue] = useState(null); // Tallennetaan valittu dropdown valikon arvo
+  const [junaData, setJunaData] = useState([]); // Tallennetaan junatiedot flatlistiin.
+  const [asemaDataValikkoon, setAsemaData] = useState([]); // Tallennetaan asematiedot dropdown valikkoa varten
 
-    // Hakee asematiedot dropdown valikkoon.
-    useEffect(() => {
-      const asemat = async () => {
-        try {
-          const asemalista = await stations(); // Funktio palauttaa jokaisen aseman nimen ja lyhenteen
-          setAsemaData(asemalista); 
-        } catch(error){
-          console.log(error);
-        }
-      };
-      asemat(); // Aloittaa asemahaun heti kun käyttäjä navigoi oikealle sivulle äpissä. 
-    }, []);
+  // Hakee asematiedot dropdown valikkoon.
+  useEffect(() => {
+    const asemat = async () => {
+      try {
+        const asemalista = await stations(); // Funktio palauttaa jokaisen aseman nimen ja lyhenteen
+        setAsemaData(asemalista);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    asemat(); // Aloittaa asemahaun heti kun käyttäjä navigoi oikealle sivulle äpissä. 
+  }, []);
 
-    // Hakee junatiedot valitulta asemalta
-    useEffect(() => {
-      if (value){
-        liveTrains(value) // funktio saa parametriksi dropdownista valitun aseman lyhenteen
+  // Hakee junatiedot valitulta asemalta
+  useEffect(() => {
+    if (value) {
+      liveTrains(value) // funktio saa parametriksi dropdownista valitun aseman lyhenteen
         .then((data) => {
           setJunaData(data); // Päivittää junatiedot muuttujaan
         })
-        .catch((error)=>{
+        .catch((error) => {
           console.log(error)
         });
-      }
-    }, [value]);
-
-  // Flatlistin sisältö
-  const renderTrainItem = ({ item }) => (
-    <View style={styles.trainItem}>
-      <Text style={styles.trainText}>{item.trainType} {item.trainNumber}</Text>
-      {item.timetable.map((time, index) => (
-        <View key={index}>
-          <Text style={styles.trainText}>{time.type}</Text>
-          <Text style={styles.trainText}>Raide: {time.commercialTrack}</Text>
-          <Text style={styles.trainText}>Aikataulu: {time.scheduledTime}. {time.status}</Text>
-          {/* <Text style={styles.trainText}></Text> */}
-          <Text style={styles.trainText}>-----------------------------</Text>
-        </View>
-      ))}
-    </View>
-  );
-
-    return (
-      <View style = {styles.container}>
-      <Dropdown
-        style={styles.dropdown}
-        placeholderStyle={styles.placeholderStyle}
-        selectedTextStyle={styles.selectedTextStyle}
-        inputSearchStyle={styles.inputSearchStyle}
-        iconStyle={styles.iconStyle}
-        data={asemaDataValikkoon}
-        search
-        maxHeight={300}
-        labelField="label" // Jos muuttaa niin pitää muuttaa myös stations funktioon!
-        valueField="value" // Jos muuttaa niin pitää muuttaa myös stations funktioon!
-        placeholder="Select item"
-        searchPlaceholder="Search..."
-        value={value}
-        onChange={(item) => {
-          setValue(item.value);
-        }}
-
-      />
-      <FlatList
-          data={junaData}
-          renderItem={renderTrainItem}
-          keyExtractor={(item, index) => `${item.trainNumber}-${index}`}
-        />
-    </View>
-  )};
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingTop: 50,
-      paddingHorizontal: 16,
-    },
-    dropdown: {
-      margin: 16,
-      height: 50,
-      backgroundColor: 'white',
-      borderRadius: 12,
-      padding: 12,
-      shadowColor: '#000',
-      shadowOffset: {
-        width: 0,
-        height: 1,
-      },
-      shadowOpacity: 0.2,
-      shadowRadius: 1.41,
-
-      elevation: 2,
-    },
-    icon: {
-      marginRight: 5,
-    },
-    item: {
-      padding: 17,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-    },
-    textItem: {
-      flex: 1,
-      fontSize: 16,
-    },
-    placeholderStyle: {
-      fontSize: 16,
-    },
-    selectedTextStyle: {
-      fontSize: 16,
-    },
-    iconStyle: {
-      width: 20,
-      height: 20,
-    },
-    inputSearchStyle: {
-      height: 40,
-      fontSize: 16,
-    },
-    trainText: {
-      color: 'white'
     }
-  });
+  }, [value]);
 
 
-  export default DropdownComponent;
+  const selectStation = (item, navigation) => {
+
+        navigation.navigate('Home', {
+
+          station: item
+    })}
+// Flatlistin sisältö
+const renderTrainItem = ({ item }) => (
+  <View style={styles.trainItem}>
+    <Text style={styles.trainText}>{item.trainType} {item.trainNumber}</Text>
+    {item.timetable.map((time, index) => (
+      <View key={index}>
+        <Text style={styles.trainText}>{time.type}</Text>
+        <Text style={styles.trainText}>Raide: {time.commercialTrack}</Text>
+        <Text style={styles.trainText}>Aikataulu: {time.scheduledTime}. {time.status}</Text>
+        {/* <Text style={styles.trainText}></Text> */}
+        <Text style={styles.trainText}>-----------------------------</Text>
+      </View>
+    ))}
+  </View>
+);
+
+return (
+  <View style={styles.container}>
+    <Dropdown
+      style={styles.dropdown}
+      placeholderStyle={styles.placeholderStyle}
+      selectedTextStyle={styles.selectedTextStyle}
+      inputSearchStyle={styles.inputSearchStyle}
+      iconStyle={styles.iconStyle}
+      data={asemaDataValikkoon}
+      search
+      maxHeight={300}
+      labelField="label" // Jos muuttaa niin pitää muuttaa myös stations funktioon!
+      valueField="value" // Jos muuttaa niin pitää muuttaa myös stations funktioon!
+      placeholder="Select item"
+      searchPlaceholder="Search..."
+      value={value}
+      onChange={(item) => {
+        setValue(item.value);
+        selectStation(item.label, navigation)
+      }}
+
+    />
+    <FlatList
+      data={junaData}
+      renderItem={renderTrainItem}
+      keyExtractor={(item, index) => `${item.trainNumber}-${index}`}
+    />
+
+  </View>
+)};
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  dropdown: {
+    margin: 16,
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 12,
+    padding: 12,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  item: {
+    padding: 17,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  textItem: {
+    flex: 1,
+    fontSize: 16,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+  trainText: {
+    color: 'white'
+  }
+});
+
+
+export default DropdownComponent;
