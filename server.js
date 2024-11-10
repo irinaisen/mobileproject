0cd40837cb44da43fc9f745e50d3a8c2a57ef6d2
Irina Päivöke <122278529+irinaisen@users.noreply.github.com>
2024-11-10 15:05:11 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.json());
     
     // var MongoClient = require('mongodb').MongoClient;
     const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-     var uri="mongodb+srv://irina20103:VRTrains@cluster0.muup47x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+     var uri="mongodb+srv://irina20103:VRTrains@cluster0.muup47x.mongodb.net/trains?retryWrites=true&w=majority&appName=Cluster0";
+
     
     const client = new MongoClient(uri,  {
         serverApi: {
@@ -48,12 +49,17 @@ app.use(express.json());
     //Responding by returnig the data added into html form. Here name and message should
     //be the names of the html form fields
     app.post('/submit', (req, res) => {
-        console.log({
+        console.log("Tadaa");
+        let comment=({
           name: req.body.name,
           message: req.body.message,
         });
+        addOneComment(comment).then(()=>readallComment().then(z=>res.send(z)));
         res.send('Kiitos viestistäsi '+req.body.name);
     });
+
+
+
     
     //This is debugging function
     //A JSON string is sent from an html form and sent by AJAX as JSON string
@@ -86,6 +92,10 @@ app.use(express.json());
     app.get('/getallweather', (req, res) => {
         readall().then(z=>res.send(z));
     });
+
+    app.get('/getallcomment', (req, res) => {
+        readallComment().then(z=>res.send(z));
+    });
     
     app.delete('/deleteoneweather/:id', (req, res) => {
         const id=req.params['id'];
@@ -103,6 +113,22 @@ app.use(express.json());
         );
         console.log("deleted");
     });
+
+    async function addOneComment(comment) {
+        console.log("addOneComment started");
+        try {
+          // Connect the client to the server (optional starting in v4.7)
+          await client.connect();
+          await client.db("trains").collection("comment").insertOne(comment);
+        }
+        catch(e){
+            console.log("Lisäyspoikkeus");
+        } 
+        //finally is run ALWAYS - even if there is a return before
+        finally {
+          await client.close();
+        }
+    }
     
     
     async function addOneWeather(weather) {
@@ -117,6 +143,35 @@ app.use(express.json());
         } 
         //finally is run ALWAYS - even if there is a return before
         finally {
+          await client.close();
+        }
+    }
+
+    async function readallComment() {
+        console.log("readallcomment started");
+        const options = {
+            // Sort returned documents in ascending order by weight
+            sort: { _id: 1 },
+            // Include all the fields into the result
+            projection: { _id: 1, comment: 1},
+          };    
+        try {
+        let arr=new Array();
+          await client.connect();
+          result=client.db("trains").collection("comment").find({}, options);
+       
+          for await (const doc of result) {
+            console.log(doc._id);//Debugging
+            arr.push(doc);
+          }
+          return arr;
+        }
+        catch(e){
+            console.log("Poikkeus");
+        } 
+        //finally is run ALWAYS - even if there is a return before
+        finally {
+          // Ensures that the client will close when you finish/error
           await client.close();
         }
     }
